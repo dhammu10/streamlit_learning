ab3c84eb90669e7c2dda55c08a37e40f685bcd39
dhammu10 <dhammdipmodak58@gmail.com>
2025-09-05 14:01:57 +0530

    Second Day

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ lang = st.selectbox("Select Language", ["None", "Java", "C++", "Python", "JavaSc
 st.write(f"your Selected Language: {lang}")
 
 st.success(f"your selected language : {lang}") 
+
+masala = st.button("Add Masala")
+if masala:
+    st.success("Adding Masala")
+
+
+gender = st.radio("select gender: ", ["Male", "Female", "Other"])
+
+st.slider('give rating', 0, 10, 1)
+
+st.text_input("Enter Your name")
+st.number_input("Enter your age")
+st.date_input("select your birth day")
