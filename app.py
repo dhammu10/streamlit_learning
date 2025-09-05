be8f7b265a77544f311974ff0003404e96c4ddaa
dhammu10 <dhammdipmodak58@gmail.com>
2025-09-05 16:12:46 +0530

    day thired

--- app.py
+++ app.py
@@ -1,30 +1,107 @@
 import streamlit as st
-import datetime
+# from PIL import Image
 
-st.title("Age Calculater")
-# st.subheader("Welcome Language Picker")
-# lang = st.selectbox("Select Language", ["None", "Java", "C++", "Python", "JavaScript"])
-# st.write(f"your Selected Language: {lang}")
+st.title("Streamlit Learning")
 
-# st.success(f"your selected language : {lang}") 
+col1, col2, col3 = st.columns(3)
 
-# masala = st.button("Add Masala")
-# if masala:
-#     st.success("Adding Masala")
+with col1:
+    st.header("Wield Fox")
+
+    st.markdown(
+        """
+        <style>
+        .custom-img img {
+            height: 300px !important;
+            width:200px !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown(
+        '<div class="custom-img"><img src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?w=600"></div>',
+        unsafe_allow_html=True
+    )
+
+    st.write("This is a fox image take from unplash")
 
 
-# gender = st.radio("select gender: ", ["Male", "Female", "Other"])
 
-# st.slider('give rating', 0, 10, 1)
+with col2:
+    st.header("wild panchhi")
 
-# st.text_input("Enter Your name")
-# st.number_input("Enter your age")
-# st.date_input("select your birth day")
+    st.markdown("""
+                
+            <style>
+                .costom-img img{
+                    height: 300px !important;
+                    width: 200px !important;
+                }
+            </style>
+                
+                """,
+               unsafe_allow_html=True 
+            )
+    
+
+    st.markdown("""
+            <div class = "costom-img"><img src="https://images.unsplash.com/photo-1497206365907-f5e630693df0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YW5pbWFsfGVufDB8fDB8fHww"> </div>
+                
+            """,
+                unsafe_allow_html=True
+                )
+    st.write("We get wiled pancchi from unplash")
 
 
-user_date = st.date_input("Enter Your age")
-current_date = datetime.date.today()
+with col3:
+    st.header("wild panchhi")
 
-user_age = current_date.year - user_date.year
+    st.markdown("""
+                
+            <style>
+                .costom-img img{
+                    height: 300px !important;
+                    width: 200px !important;
+                }
+            </style>
+                
+                """,
+               unsafe_allow_html=True 
+            )
+    
 
-st.success(f"your age is {user_age}")
+    st.markdown("""
+            <div class = "costom-img"><img src="https://images.unsplash.com/photo-1497206365907-f5e630693df0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YW5pbWFsfGVufDB8fDB8fHww"> </div>
+                
+            """,
+                unsafe_allow_html=True
+                )
+    st.write("We get wiled pancchi from unplash")
+
+
+
+first_name = st.sidebar.text_input("Enter your first name")
+last_name = st.sidebar.text_input("Enter your last name")
+gender = st.sidebar.selectbox("Choose your gender", ["Gender", "Male", "Female", "Other"])
+
+
+with st.expander("check Addmission Process"):
+    st.write(""" 
+             1. apply collages that you want <br>
+             2. then check a allotment collage <br>
+             3. then verify your document<br>
+             4. verify your all document<br>
+             5. Paid collage fees
+             6. attend a letures """,
+             
+             unsafe_allow_html=True)
+
+
+
+st.markdown("### Welcome chai app")
+st.markdown(">are you tied")
+
+
+ 
